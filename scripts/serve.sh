aa1d05c0c455f366e9bbbcd6f92bd2c981c971ff
Austin Scola <austinscola@gmail.com>
2021-07-25 19:48:39 -0400

    Add serve script
    Add a script to serve the website.

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -11,14 +11,15 @@ NAME="austin_scola_website"
 TAG="$(git rev-parse HEAD)"
 NAME_TAG="${NAME}:${TAG}"
 
-COMMAND="bundle exec jekyll build"
+COMMAND="bundle exec jekyll serve --host 0.0.0.0"
 
 MOUNT_SOURCE="${REPO_ROOT}/docs"
 MOUNT_DEST=/website/docs
-echo "Building the site..."
+echo "Serving the site..."
 docker run \
     --mount type=bind,src="${MOUNT_SOURCE}",dst="${MOUNT_DEST}" \
+    --expose=4000\
+    --publish 127.0.0.1:4000:4000 \
     --entrypoint /bin/bash \
     "${NAME_TAG}" \
     -c "${COMMAND}"
-echo "Successfully built the site."
